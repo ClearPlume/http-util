b2d4ac5b98468e7d28e51aeb83fb6dfaa54ce2e8
the_FallenAngel <the.fallenangel.965@gmail.com>
2023-09-08 10:32:48 +0800

    wip: 版本升级 0.0.4 -> 0.0.5
    新增SSL配置
    SSLHostnameVerifier
    SSLContext
    SSLSocketFactory

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 }
 
 group = "net.fallingangel"
-version = "0.0.4"
+version = "0.0.5"
 
 repositories {
     mavenCentral()
@@ -47,7 +47,7 @@ publishing {
             from(components["java"])
             groupId = "net.fallingangel"
             artifactId = "http-util"
-            version = "0.0.4"
+            version = "0.0.5"
 
             pom {
                 name.set("HttpUtil")
