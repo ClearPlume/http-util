c66e7039d86f27920752d7e2c3cbf97c5a849dc5
the_FallenAngel <the.fallenangel.965@gmail.com>
2023-11-22 10:32:19 +0800

    wip: 删除Pair

--- src/main/kotlin/net/fallingangel/httputil/Pair.kt
+++
@@ -1,8 +0,0 @@
-package net.fallingangel.httputil
-
-data class Pair<K, V>(val key: K, val value: V) {
-    companion object {
-        @JvmStatic
-        fun <K, V> of(key: K, value: V) = Pair(key, value)
-    }
-}
